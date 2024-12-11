7086943599bf5ae5e10e398f682885ec971853b2
JoaquinMarmol <joaquinmarmolnecochea@gmail.com>
2024-12-11 00:31:10 -0300

    prueba

--- helpers/formatCustomDate.jsx
+++
@@ -1,7 +0,0 @@
-import { parseISO, format } from 'date-fns';
-
-export default function formatCustomDate(isoDateString, id) {
-  const date = parseISO(isoDateString);
-  return format(date, "EEEE, MMMM do");
-}
-
